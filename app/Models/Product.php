606fbbbec685e3353aca323e421dc17fd24be0b1
egatapova <gatapova.workspace21@gmail.com>
2023-03-02 22:48:40 +0300

    fix

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,16 +11,4 @@ class Product extends Model
 
     protected $fillable = ['name', 'amount'];
 
-    public function getCurrentPriceAttribute()
-    {
-        return 100;
-    }
-
-    public function getAmount($date)
-    {
-        $suppliesAmount = $this->hasMany(Supply::class, 'product_id')->whereDate('supply_date', '=', $date)->sum('amount');
-        $preordersAmount = $this->hasMany(Preorder::class, 'product_id')->whereDate('preorder_date', '=', $date)->sum('amount');
-        $amount = $suppliesAmount - $preordersAmount;
-        return $amount;
-    }
 }
